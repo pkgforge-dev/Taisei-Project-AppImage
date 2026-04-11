e406186372a46a0cfe865775a9825ab2b384693b
Link4Electronics <link4electronics@gmail.com>
2026-04-11 20:54:44 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,22 +5,22 @@ set -eu
 ARCH=$(uname -m)
 export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/taisei-project/taisei/refs/heads/master/xdg/icons/taisei-256.png
-export DESKTOP=/usr/share/applications/org.taisei_project.Taisei.desktop
+export DESKTOP=https://raw.githubusercontent.com/taisei-project/taisei/refs/heads/master/xdg/org.taisei_project.Taisei.desktop
 export STARTUPWMCLASS=org.taisei_project.Taisei
 export DEPLOY_OPENGL=1
-export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/taisei
-
-# Additional changes can be done in between here
+quick-sharun /usr/local/taisei
+mkdir -p ./AppDir/bin
+rm -rf ./AppDir/bin/data
+cp -a /usr/local/data ./AppDir/bin/data
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the app normally quits before that time
 # then skip this or check if some flag can be passed that makes it stay open
-quick-sharun --test ./dist/*.AppImage
+quick-sharun --simple-test ./dist/*.AppImage
