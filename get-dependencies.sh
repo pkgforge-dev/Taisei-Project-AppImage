e091b738afec3a4f65eabb6ccbc481ac2ea141de
Link4Electronics <link4electronics@gmail.com>
2026-09-10 15:40:05 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -18,7 +18,7 @@ pacman -Syu --noconfirm \
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano libdecor-mini
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini opus-mini
 
 # Comment this out if you need an AUR package
 make-aur-package cglm
